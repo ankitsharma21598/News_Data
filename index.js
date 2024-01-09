321481129fb03d7f7f0501337c598ae7d37c77f4
My java <ankit.sharma21598@gmail.com>
2024-01-10 00:28:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const jsonServer = require("json-server");
-const cors = require("cors"); // Import the cors middleware
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
-
-// Use the cors middleware
-server.use(cors());
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
